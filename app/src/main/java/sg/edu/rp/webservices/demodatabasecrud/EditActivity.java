16d18d7769ccd79af950e828874e55ddfea89cbd
17010322 <17010322@myrp.edu.sg>
2020-05-22 16:39:23 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/demodatabasecrud/EditActivity.java
@@ -0,0 +1,61 @@
+package sg.edu.rp.webservices.demodatabasecrud;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends MainActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_activity);
+
+        //initialize the variables with UI here
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+//UPDATE.........................................................................................
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                dbh.close();
+            }
+        });
+        //UPDATE END........................................................................................
+
+        //DELETE..................................................................................................
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+                dbh.close();
+            }
+        });
+
+
+
+        //DELETE END................................................................................
+
+    }
+
+
+}
+
